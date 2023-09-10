9d39ca4165f7ad1396bc193868931631f5e128a4
Marcus Weber <marcus.wallau@gmail.com>
2023-09-10 16:00:26 +0200

    feat: add k9s plugins

--- k9s/default.nix
+++ k9s/default.nix
@@ -5,6 +5,7 @@
   ];
 
   xdg.configFile."k9s/skin.yml" = { source = ./skin.yml; };
+  xdg.configFile."k9s/plugin.yml".source = ./k9s-plugin.yml;
 
   programs.bash.sessionVariables = {
     K9SCONFIG = "$XDG_CONFIG_HOME/k9s";
